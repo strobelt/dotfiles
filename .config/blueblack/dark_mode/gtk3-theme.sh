a4d8618d3e91982d16254ab761ef1eea8545812d
Luiz Strobelt <lhsperez@gmail.com>
2025-10-29 15:05:38 -0300

    2510

---
+++ .config/blueblack/dark_mode/gtk3-theme.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yes | cp -rf ~/.config/gtk-3.0/settings-dark.ini ~/.config/gtk-3.0/settings.ini
---
+++ .config/blueblack/light_mode/gtk3-theme.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+yes | cp -rf ~/.config/gtk-3.0/settings-light.ini ~/.config/gtk-3.0/settings.ini
